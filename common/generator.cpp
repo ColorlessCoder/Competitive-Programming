16462d6d04e2cd2214eeb0f9febc9d5d6f7ad6aa
Munawar Hussain Shakil <munawar.hussain@enosisbd.com>
2018-08-08 00:32:54 +0600

    #1 Structure has been build

---
+++ common/generator.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define LL long long int
+#define ULL unsigned long long int
+#define sf(a) scanf("%d",&a)
+#define sl(a) scanf("%lld",&a)
+#define fr first
+#define sc second
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define vi vector<int>
+#define vll vector<LL>
+#define vpii  vector<pii>
+#define rep1(a,b) for(int a=1;a<=b;a++)
+#define rep2(a,b) for(int a=0;a<b;a++)
+#define CLR(a,b) memset(a,b,sizeof(a))
+#define Clear(a,b) memset(a,b,sizeof(a))
+#define pb push_back
+#define mp make_pair
+#define mod 1000000007
+#define  LSOne(S) (S&(-S))
+#define all(a) a.begin(),a.end()
+#define Prime 31
+using namespace std;
+#define maxn 200100
+#define INF 1<<30
+#define mMax 200005
+#define nMax 1005
+#define SZ(a) a.size()
+int main()
+{
+    
+    return 0;
+}
---
+++ sublime config/instructions.md
@@ -0,0 +1,6 @@
+1. Open Sublime.
+2. Go to 'Tools'.
+3. `Build System`.
+4. `New Build System...`.
+5. Copy and paste the codes into the new opened file.
+6. Save the file.
