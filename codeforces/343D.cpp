a22ae7b07cd0c48f8b009cf58f99b06a3038ab2a
Munawar Hussain Shakil <munawar.hussain@enosisbd.com>
2018-08-08 23:58:05 +0600

    #3 not submitted yet

--- codeforces/343D.cpp
+++ codeforces/343D.cpp
@@ -32,90 +32,138 @@ using namespace std;
 void printVector(vi a) {for (int ii = 0; ii < SZ(a); ii++) cout << a[ii] << endl;}
 
 class SegmentTree {
-private: vi st, A;
+private: vi fill, empty;
 	int n;
 	int left (int p) { return p << 1; }
 	int right(int p) { return (p << 1) + 1; }
-	int rmq(int p, int L, int R, int i, int j) {
-		if (i >  R || j <  L) return -1;
-		if (L >= i && R <= j) return st[p];
 
-		int p1 = rmq(left(p) , L              , (L + R) / 2, i, j);
-		int p2 = rmq(right(p), (L + R) / 2 + 1, R          , i, j);
+	void propagate(int p, int L, int R) {
+		if (fill[p] > -1) {
+			int p1 = left(p);
+			int p2 = right(p);
+			fill[p1] = max(fill[p1], fill[p]);
+			fill[p2] = max(fill[p2], fill[p]);
+			fill[p] = -1;
+		}
+	}
 
-		if (p1 == -1) return p2;
-		if (p2 == -1) return p1;
-		return (A[p1] <= A[p2]) ? p1 : p2;
+	int maxfillquery(int p, int L, int R, int i) {
+		if (i > R || i < L) return -1;
+		if (L == i && R == i) return fill[p];
+		propagate(p, L, R);
+		int p1 = maxfillquery(left(p) , L              , (L + R) / 2, i);
+		int p2 = maxfillquery(right(p), (L + R) / 2 + 1, R          , i);
+		return max(p1, p2);
+	}
+
+	int maxemptyquery(int p, int L, int R, int i, int j) {
+		if (i > R || j < L) return -1;
+		if (L >= i && R <= j) return empty[p];
+
+		int p1 = maxemptyquery(left(p) , L              , (L + R) / 2, i, j);
+		int p2 = maxemptyquery(right(p), (L + R) / 2 + 1, R          , i, j);
+		return max(p1, p2);
 	}
 
 	int update_point(int p, int L, int R, int idx, int new_value) {
 		int i = idx, j = idx;
 
 		if (i > R || j < L)
-			return st[p];
+			return empty[p];
 
 		if (L == i && R == j) {
-			A[i] = new_value;
-			return st[p] = L;
+			return empty[p] = new_value;
 		}
 
 		int p1, p2;
 		p1 = update_point(left(p) , L              , (L + R) / 2, idx, new_value);
 		p2 = update_point(right(p), (L + R) / 2 + 1, R          , idx, new_value);
 
-		return st[p] = (A[p1] >= A[p2]) ? p1 : p2;
+		return empty[p] = max(p1, p2);
+	}
+
+	int update_range(int p, int L, int R, int i, int j, int new_value) {
+		if (i > R || j < L)
+			return fill[p];
+
+		if (i <= L && R <= j) {
+			return fill[p] = new_value;
+		}
+		propagate(p, L, R);
+		int p1, p2;
+		p1 = update_range(left(p) , L              , (L + R) / 2, i, j, new_value);
+		p2 = update_range(right(p), (L + R) / 2 + 1, R          , i, j, new_value);
+
+		return fill[p] = max(p1, p2);
 	}
 
 public:
 	SegmentTree(int a, int b = 0) {
 		n = a;
-		A.assign(n, b);
-		st.assign(4 * n, b);
+		fill.assign(4 * n, b);
+		empty.assign(4 * n, b);
 	}
 
-	int rmq(int i, int j) { return A[rmq(1, 0, n - 1, i, j)]; }
+	int maxfillquery(int i) { return maxfillquery(1, 0, n - 1, i); }
+	int maxemptyquery(int i, int j) { return maxemptyquery(1, 0, n - 1, i, j); }
 
 	int update_point(int idx, int new_value) {
 		return update_point(1, 0, n - 1, idx, new_value);
 	}
+
+	int update_range(int i, int j, int new_value) {
+		return update_range(1, 0, n - 1, i, j, new_value);
+	}
 };
 
 int timeCount = 0;
-struct entry{
+struct entry {
 	int start;
 	int end;
-	entry(){}
-	entry(int a,int b){
-		start=a;
-		end= b;
+	entry() {}
+	entry(int a, int b) {
+		start = a;
+		end = b;
 	}
 } visitTime[500100];
 
 vector<vi> adjList;
 
-void dfs(int u,int par) {
+void dfs(int u, int par) {
 	visitTime[u].start = timeCount++;
-	for(auto v:adjList[u])
-		if(v!=par){
-			dfs(v,u);
+	for (auto v : adjList[u])
+		if (v != par) {
+			dfs(v, u);
 		}
-	visitTime[u].end=timeCount++;
+	visitTime[u].end = timeCount++;
 }
 
 int main()
 {
 	int n;
-	cin>>n;
-	adjList.assign(n+10,vi());
-	int u,v;
-	rep1(i,n-1){
-		scanf("%d %d",&u,&v);
+	cin >> n;
+	adjList.assign(n + 10, vi());
+	int u, v;
+	rep1(i, n - 1) {
+		scanf("%d %d", &u, &v);
 		adjList[u].push_back(v);
 		adjList[v].push_back(u);
 	}
-	dfs(1,0);
-	// rep1(i,n){
-	// 	cout<<visitTime[i].start<<" "<<visitTime[i].end<<endl;
-	// }
+	dfs(1, 0);
+	SegmentTree A(timeCount + 2, -1);
+	int q;
+	cin >> q;
+	rep1(i, q) {
+		scanf("%d %d", &u, &v);
+		if (u == 1) {
+			A.update_range(visitTime[v].start, visitTime[v].end, i);
+		}	else if (u == 2) {
+			A.update_point(visitTime[v].start, i);
+		}	else {
+			int x = A.maxemptyquery(visitTime[v].start, visitTime[v].end);
+			int y = A.maxfillquery(visitTime[v].start);
+			printf("%d\n",  (x > y || y == -1) ? 0 : 1);
+		}
+	}
 	return 0;
 }
