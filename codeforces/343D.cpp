4cfbdc0a944ac0dc2d003cfda5df2a98512ba3df
Munawar Hussain Shakil <munawar.hussain@enosisbd.com>
2018-08-09 17:51:10 +0600

    #3 Indentation Change

--- codeforces/343D.cpp
+++ codeforces/343D.cpp
@@ -2,43 +2,51 @@
 #define ll long long int
 #define LL long long int
 #define ULL unsigned long long int
-#define sf(a) scanf("%d",&a)
-#define sl(a) scanf("%lld",&a)
+#define sf(a) scanf("%d", &a)
+#define sl(a) scanf("%lld", &a)
 #define fr first
 #define sc second
-#define pii pair<int,int>
-#define pll pair<LL,LL>
+#define pii pair<int, int>
+#define pll pair<LL, LL>
 #define vi vector<int>
 #define vll vector<LL>
-#define vpii  vector<pii>
-#define rep1(a,b) for(int a=1;a<=b;a++)
-#define rep2(a,b) for(int a=0;a<b;a++)
-#define CLR(a,b) memset(a,b,sizeof(a))
-#define Clear(a,b) memset(a,b,sizeof(a))
+#define vpii vector<pii>
+#define rep1(a, b) for (int a = 1; a <= b; a++)
+#define rep2(a, b) for (int a = 0; a < b; a++)
+#define CLR(a, b) memset(a, b, sizeof(a))
+#define Clear(a, b) memset(a, b, sizeof(a))
 #define pb push_back
 #define mp make_pair
 #define mod 1000000007
-#define  LSOne(S) (S & (-S))
-#define all(a) a.begin(),a.end()
+#define LSOne(S) (S & (-S))
+#define all(a) a.begin(), a.end()
 #define Prime 31
 using namespace std;
 #define maxn 200100
-#define INF 1<<29
-#define inf 1ll<<62
+#define INF 1 << 29
+#define inf 1ll << 62
 #define mMax 20005
 #define nMax 2505
 #define SZ(a) a.size()
-#define  Unique(A) (A.resize(distance(A.begin(),unique(all(A)))))
-void printVector(vi a) {for (int ii = 0; ii < SZ(a); ii++) cout << a[ii] << endl;}
+#define Unique(A) (A.resize(distance(A.begin(), unique(all(A)))))
+void printVector(vi a)
+{
+	for (int ii = 0; ii < SZ(a); ii++)
+		cout << a[ii] << endl;
+}
 
-class SegmentTree {
-private: vi fill, empty;
+class SegmentTree
+{
+private:
+	vi fill, empty;
 	int n;
-	int left (int p) { return p << 1; }
+	int left(int p) { return p << 1; }
 	int right(int p) { return (p << 1) + 1; }
 
-	void propagate(int p, int L, int R) {
-		if (fill[p] > -1) {
+	void propagate(int p, int L, int R)
+	{
+		if (fill[p] > -1)
+		{
 			int p1 = left(p);
 			int p2 = right(p);
 			fill[p1] = max(fill[p1], fill[p]);
@@ -47,46 +55,56 @@ private: vi fill, empty;
 		}
 	}
 
-	int maxfillquery(int p, int L, int R, int i) {
-		if (i > R || i < L) return -1;
-		if (L == i && R == i) return fill[p];
+	int maxfillquery(int p, int L, int R, int i)
+	{
+		if (i > R || i < L)
+			return -1;
+		if (L == i && R == i)
+			return fill[p];
 		propagate(p, L, R);
-		int p1 = maxfillquery(left(p) , L              , (L + R) / 2, i);
-		int p2 = maxfillquery(right(p), (L + R) / 2 + 1, R          , i);
+		int p1 = maxfillquery(left(p), L, (L + R) / 2, i);
+		int p2 = maxfillquery(right(p), (L + R) / 2 + 1, R, i);
 		return max(p1, p2);
 	}
 
-	int maxemptyquery(int p, int L, int R, int i, int j) {
-		if (i > R || j < L) return -1;
-		if (L >= i && R <= j) return empty[p];
+	int maxemptyquery(int p, int L, int R, int i, int j)
+	{
+		if (i > R || j < L)
+			return -1;
+		if (L >= i && R <= j)
+			return empty[p];
 
-		int p1 = maxemptyquery(left(p) , L              , (L + R) / 2, i, j);
-		int p2 = maxemptyquery(right(p), (L + R) / 2 + 1, R          , i, j);
+		int p1 = maxemptyquery(left(p), L, (L + R) / 2, i, j);
+		int p2 = maxemptyquery(right(p), (L + R) / 2 + 1, R, i, j);
 		return max(p1, p2);
 	}
 
-	int update_point(int p, int L, int R, int idx, int new_value) {
+	int update_point(int p, int L, int R, int idx, int new_value)
+	{
 		int i = idx, j = idx;
 
 		if (i > R || j < L)
 			return empty[p];
 
-		if (L == i && R == j) {
+		if (L == i && R == j)
+		{
 			return empty[p] = new_value;
 		}
 
 		int p1, p2;
-		p1 = update_point(left(p) , L              , (L + R) / 2, idx, new_value);
-		p2 = update_point(right(p), (L + R) / 2 + 1, R          , idx, new_value);
+		p1 = update_point(left(p), L, (L + R) / 2, idx, new_value);
+		p2 = update_point(right(p), (L + R) / 2 + 1, R, idx, new_value);
 
 		return empty[p] = max(p1, p2);
 	}
 
-	void update_range(int p, int L, int R, int i, int j, int new_value) {
+	void update_range(int p, int L, int R, int i, int j, int new_value)
+	{
 		if (i > R || j < L)
-			return ;
+			return;
 
-		if (i <= L && R <= j) {
+		if (i <= L && R <= j)
+		{
 			fill[p] = new_value;
 			return;
 		}
@@ -94,12 +112,13 @@ private: vi fill, empty;
 		propagate(p, L, R);
 
 		int p1, p2;
-		update_range(left(p) , L              , (L + R) / 2, i, j, new_value);
-		update_range(right(p), (L + R) / 2 + 1, R          , i, j, new_value);
+		update_range(left(p), L, (L + R) / 2, i, j, new_value);
+		update_range(right(p), (L + R) / 2 + 1, R, i, j, new_value);
 	}
 
 public:
-	SegmentTree(int a, int b = 0) {
+	SegmentTree(int a, int b = 0)
+	{
 		n = a;
 		fill.assign(4 * n, b);
 		empty.assign(4 * n, b);
@@ -108,21 +127,25 @@ public:
 	int maxfillquery(int i) { return maxfillquery(1, 0, n - 1, i); }
 	int maxemptyquery(int i, int j) { return maxemptyquery(1, 0, n - 1, i, j); }
 
-	int update_point(int idx, int new_value) {
+	int update_point(int idx, int new_value)
+	{
 		return update_point(1, 0, n - 1, idx, new_value);
 	}
 
-	void update_range(int i, int j, int new_value) {
+	void update_range(int i, int j, int new_value)
+	{
 		update_range(1, 0, n - 1, i, j, new_value);
 	}
 };
 
 int timeCount = 0;
-struct entry {
+struct entry
+{
 	int start;
 	int end;
 	entry() {}
-	entry(int a, int b) {
+	entry(int a, int b)
+	{
 		start = a;
 		end = b;
 	}
@@ -130,10 +153,12 @@ struct entry {
 
 vector<vi> adjList;
 
-void dfs(int u, int par) {
+void dfs(int u, int par)
+{
 	visitTime[u].start = timeCount++;
 	for (auto v : adjList[u])
-		if (v != par) {
+		if (v != par)
+		{
 			dfs(v, u);
 		}
 	visitTime[u].end = timeCount++;
@@ -145,25 +170,32 @@ int main()
 	cin >> n;
 	adjList.assign(n + 10, vi());
 	int u, v;
-	rep1(i, n - 1) {
+	rep1(i, n - 1)
+	{
 		scanf("%d %d", &u, &v);
 		adjList[u].push_back(v);
 		adjList[v].push_back(u);
 	}
 	dfs(1, 0);
-	SegmentTree A(timeCount , -1);
+	SegmentTree A(timeCount, -1);
 	int q;
 	cin >> q;
-	rep1(i, q) {
+	rep1(i, q)
+	{
 		scanf("%d %d", &u, &v);
-		if (u == 1) {
+		if (u == 1)
+		{
 			A.update_range(visitTime[v].start, visitTime[v].end, i);
-		}	else if (u == 2) {
+		}
+		else if (u == 2)
+		{
 			A.update_point(visitTime[v].start, i);
-		}	else {
+		}
+		else
+		{
 			int x = A.maxemptyquery(visitTime[v].start, visitTime[v].end);
 			int y = A.maxfillquery(visitTime[v].start);
-			printf("%d\n",  (x > y || y == -1) ? 0 : 1);
+			printf("%d\n", (x > y || y == -1) ? 0 : 1);
 		}
 	}
 	return 0;
